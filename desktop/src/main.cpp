e6720baa4aa7c197d8f9d0c270906d061190db74
w1zZzyy <dkilikki22@gmail.com>
2025-08-04 19:18:40 +0300

    main refactored

--- desktop/src/main.cpp
+++ desktop/src/main.cpp
@@ -1,4 +1,16 @@
+#include "ui/window/view.hpp"
+
 int main()
 {
+    desktop::ui::WindowRenderer window;
     
+    while(window.IsOpen()) 
+    {
+        while(auto event = window.PollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.Close();
+        }
+
+        window.Draw();
+    }
 }
