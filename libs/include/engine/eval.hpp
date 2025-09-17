42153c6ab2a062a26635ce160cd7547680766171
w1zZzyy <dkilikki22@gmail.com>
2025-09-17 20:24:09 +0300

    pestos evaluation

--- libs/include/engine/eval.hpp
+++ libs/include/engine/eval.hpp
@@ -1,48 +1,40 @@
-#pragma once
+#pragma once 
 
+#include "logic/defs.hpp"
 #include "logic/position.hpp"
-#include <stack>
 
 namespace game::engine
 {
 
-class Eval
-{
+class Evaluation {
 public:
 
     static void Setup();
-    
-    void init(const PositionFixedMemory& pos);
-    void update(logic::Move move);
 
-    int score() const;
-    
-    void push() {prev.push(data);}
-    void rollback() {assert(!prev.empty()); data = prev.top();}
-    void pop() {assert(!prev.empty()); prev.pop();}
-
-private:
-
-    void update_piece(logic::Color side, logic::Piece piece, logic::Square from, logic::Square targ);
-    void add_piece(logic::Color side, logic::Piece piece, logic::Square sqr);
-    void remove_piece(logic::Color side, logic::Piece piece, logic::Square from);
-    void castle(logic::Color side, logic::Square kf, logic::Square kt, logic::Square rf, logic::Square rt);
-
-private:
+    void Init(const PositionFixedMemory&);
+    void Update(logic::Move, logic::Color us);
+    int Score() const;
 
     struct Data {
         int mg[logic::COLOR_COUNT];
         int eg[logic::COLOR_COUNT];
         int game_phase;
     };
+    Data GetData() const noexcept {return data;}
+    void SetData(const Data& _data) noexcept {data = _data;}
 
 private:
 
-    const PositionFixedMemory* pos;
+    void addPiece(logic::Color side, logic::Piece piece, logic::Square sqr);
+    void removePiece(logic::Color side, logic::Piece piece, logic::Square from);
+    void movePiece(logic::Color side, logic::Piece piece, logic::Square from, logic::Square targ);
+    void castle(logic::Color side, logic::Square kf, logic::Square kt, logic::Square rf, logic::Square rt);
+
+private:
+
     Data data;
-    std::stack<Data> prev;
+    const PositionFixedMemory* pos;
 
 };
-
 
 }
