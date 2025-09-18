6bda22dc40de7acebc0c2872fc8f79ae65558d11
w1zZzyy <dkilikki22@gmail.com>
2025-09-18 15:04:50 +0300

    iterable

--- libs/include/logic/movelist.hpp
+++ libs/include/logic/movelist.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "position.hpp"
+#include <cstddef>
+#include <stdexcept>
 
 namespace game::logic
 {
@@ -13,12 +15,18 @@ public:
 
     template<MoveGenType MGT, StorageType ST>
     void generate(const Position<ST>& p);
+
     bool empty() const noexcept {return get_size() == 0;}
     size_t get_size() const noexcept {return curr - moves;}
-    Move operator [] (size_t i) const {
+    Move operator[] (size_t i) const {
+        if(i >= get_size()) throw std::runtime_error("out of range");
         return moves[i];
     }
-    Move* data() noexcept {return moves;}
+    
+    Move* begin() noexcept {return moves;}
+    Move* end() noexcept {return curr;}
+    const Move* begin() const noexcept {return moves;}
+    const Move* end() const noexcept {return curr;}
 
 private:
 
